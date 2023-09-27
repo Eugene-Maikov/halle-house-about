2be8edb9cb4fcdef4972a1c59eec3f446f11ef98
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-09-27 17:00:52 +0300

    feat: add feature

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,13 +1,26 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  console.log('Index scripts');
-  var swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 20,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+  var calcFeatureLine = function calcFeatureLine() {
+    var featureLine = document.querySelector('.feature__line');
+    var featureListHeight = document.querySelector('.feature__list').offsetHeight;
+    var featureLastItemHeight = document.querySelector('.feature__item:last-child').offsetHeight;
+    if (window.innerWidth > 1024) {
+      featureLine.style.height = featureListHeight - featureLastItemHeight / 2 + 'px';
+    } else {
+      featureLine.style.height = featureListHeight - featureLastItemHeight + 'px';
     }
-  });
+  };
+  calcFeatureLine();
+  window.addEventListener("resize", calcFeatureLine);
+  var featureNumbering = function featureNumbering() {
+    var featureItems = document.querySelectorAll('.feature__item');
+    featureItems.forEach(function (item, index) {
+      var number = document.createElement('div');
+      number.classList.add('feature__number');
+      number.textContent = "".concat(index + 1);
+      item.appendChild(number);
+    });
+  };
+  featureNumbering();
 });
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log('Index scripts')
 
-  //= components/swiper.js
+  const calcFeatureLine = () => {
+    const featureLine = document.querySelector('.feature__line')
+
+    const featureListHeight = document.querySelector('.feature__list').offsetHeight
+    const featureLastItemHeight = document.querySelector('.feature__item:last-child').offsetHeight
+
+    if (window.innerWidth > 1024) {
+      featureLine.style.height = (featureListHeight - (featureLastItemHeight / 2)) + 'px'
+    } else {
+      featureLine.style.height = (featureListHeight - featureLastItemHeight) + 'px'
+    }
+  }
+  calcFeatureLine()
+  window.addEventListener("resize", calcFeatureLine)
+
+
+  const featureNumbering = () => {
+    const featureItems = document.querySelectorAll('.feature__item')
+
+    featureItems.forEach((item, index) => {
+      const number = document.createElement('div')
+      number.classList.add('feature__number')
+      number.textContent = `${index + 1}`
+      item.appendChild(number)
+    })
+  }
+  featureNumbering()
 })
